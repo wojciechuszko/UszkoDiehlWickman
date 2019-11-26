01196734fc0b8a3e03326852dc680788b1200e2b
wojciechuszko <45428932+wojciechuszko@users.noreply.github.com>
2019-11-26 11:08:48 +0100

    Delete remove_nonconverged_fits.m

--- EmpiricalStudy/remove_nonconverged_fits.m
+++
@@ -1,9 +0,0 @@
-function fit_results_converged_only = remove_nonconverged_fits( fit_results )
-% This function removes all bootstrap coefficent from the results where the
-% fits did not converge, as well as the indicator for which fits converged
-
-fit_results_converged_only = fit_results;
-fit_results_converged_only.beta_bootstrap = ...
-    fit_results.beta_bootstrap( : , fit_results.is_converged_bootstrap );
-
-fit_results_converged_only = rmfield( fit_results_converged_only , 'is_converged_bootstrap' );
