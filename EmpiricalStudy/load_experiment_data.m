b13fc83fc1002317830db4b6dc5a5db4a5ae4673
wojciechuszko <45428932+wojciechuszko@users.noreply.github.com>
2019-11-26 11:08:15 +0100

    Delete load_experiment_data.m

--- EmpiricalStudy/load_experiment_data.m
+++
@@ -1,8 +0,0 @@
-function experiment_data = load_experiment_data( file_name )
-% This function loads a pre-formatted data file containing the results from
-% a functional-response experiment, and calculates the number of feeding
-% trials, as well as the scaled temperature for each trial
-
-experiment_data = load( file_name );
-experiment_data.number_of_trials = length( experiment_data.duration );
-experiment_data.scaled_temperature = calc_scaled_temperatures( experiment_data );
